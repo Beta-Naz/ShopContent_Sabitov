edcac4756a4d079e632c5fef8b21deb4e91bdfab
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-21 13:36:45 +0500

    Разработанные модели данных. Сабитов

---
+++ Model/Category.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Model
+{
+    public class Category : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Model/Item.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopContent.Model
+{
+    public class Item : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private double _price;
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        private Category _category;
+        public Category Category
+        {
+            get => _category;
+            set
+            {
+                _category = value;
+                OnPropertyChanged("Category");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
