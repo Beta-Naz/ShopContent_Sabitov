0ca6093114df4f45931b77263d88d873ce7de3c7
kenn1d <makarkibanov220@gmail.com>
2026-05-27 09:27:27 +0500

    Разработанные модели представления. Сабитов

---
+++ ViewModell/VMCategories.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ShopContent.Context;
+
+namespace ShopContent.ViewModell
+{
+    public class VMCategories : INotifyPropertyChanged
+    {
+        public ObservableCollection<CategoryContext> Categories { get; set; }
+        public VMCategories() =>
+            Categories = CategoryContext.AllCategories();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- ViewModell/VMItems.cs
+++ ViewModell/VMItems.cs
@@ -1,10 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using ShopContent.Context;
 
 namespace ShopContent.ViewModell
 {
-    public class VMItems
+    public class VMItems : INotifyPropertyChanged
     {
-        public List<ItemContext> Items;
+        public ObservableCollection<ItemContext> Items { get; set; }
+        public RelayCommand NewItem
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    ItemContext newModell = new ItemContext(true);
+                    Items.Add(newModell);
+                    MainWindow.Instance.OpenPage(new View.Add(newModell));
+                });
+            }
+        }
+        public VMItems() =>
+            Items = ItemContext.AllItems();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
